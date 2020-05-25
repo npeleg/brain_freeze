37f8462c5478889755aa52992876d4fb7ff99497
npeleg <pelegneufeld@mail.tau.ac.il>
2020-05-25 11:36:23 +0300

    test_server_client.py and test_multi_clients.py are passing

--- tests/test_multi_clients.py
+++ tests/test_multi_clients.py
@@ -1,17 +1,21 @@
 from utils.simulate_process import run_subprocess, sleep, SMALL_SAMPLE_PATH
 
+process_num = 3
+
 
 def test_multi_client():
     server_process = run_subprocess("python -m final.server run-server dummy")
     sleep(2)
-    client_process1 = run_subprocess("python -m final.client upload_sample " + SMALL_SAMPLE_PATH)
-    client_process2 = run_subprocess("python -m final.client upload_sample " + SMALL_SAMPLE_PATH)
-    client_process3 = run_subprocess("python -m final.client upload_sample " + SMALL_SAMPLE_PATH)
-    sleep(300)
-    client_process1.terminate()
-    client_process2.terminate()
-    client_process3.terminate()
+
+    processes = []
+    for i in range(process_num):
+        client_process = run_subprocess("python -m final.client upload_sample " + SMALL_SAMPLE_PATH)
+        processes.append(client_process)
+
+    for process in processes:
+        process.wait()
+        process.terminate()
+        process.communicate()
+        assert process.returncode == 0
+
     server_process.terminate()
-    assert client_process1.returncode == 0
-    assert client_process2.returncode == 0
-    assert client_process3.returncode == 0
--- tests/test_server_client.py
+++ tests/test_server_client.py
@@ -7,18 +7,14 @@ def test_client():
     server_process = run_subprocess("python -m final.server run-server dummy")
     sleep(2)
 
+    client_process = run_subprocess("python -m final.client upload_sample " + SMALL_SAMPLE_PATH)
+    client_process.wait()
+    client_process.terminate()
+    out, err = client_process.communicate()
+    assert client_process.returncode == 0
+
     return_code = upload_sample(host='127.0.0.1', port=8000, path=SMALL_SAMPLE_PATH)
     assert return_code == 0
 
-    client_process = run_subprocess("python -m final.client upload_sample " + SMALL_SAMPLE_PATH)
-    sleep(300)
-    client_process.terminate()
-    # out, err = client_process.communicate()
-    # print("client out: " + out.decode())
-    # print("client err: " + err.decode())
     server_process.terminate()
-    # out, err = server_process.communicate()
-    # print("server out: " + out.decode())
-    # print("server err: " + err.decode())
-    assert client_process.returncode == 0
 
