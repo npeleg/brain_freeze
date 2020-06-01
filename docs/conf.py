e222004c806e1023718531d385b103e744cab7c5
npeleg <pelegneufeld@mail.tau.ac.il>
2020-06-01 12:25:11 +0300

    fixed docs

--- docs/conf.py
+++ docs/conf.py
@@ -59,6 +59,4 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '_static/output-onlinepngtools.png',
-                    '_static/moon.png', '_static/spacesuit.png',
-                    '_static/system_components.png', ]
+html_static_path = ['_static']
